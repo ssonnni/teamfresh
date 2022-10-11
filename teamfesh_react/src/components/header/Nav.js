7b93674a315ca04ce4e499f10b5e1cdc148d6a9d
So Hyun Yang <91578131+ssonnni@users.noreply.github.com>
2022-10-12 04:48:10 +0900

    Nav.js css 수정

--- teamfesh_react/src/components/header/Nav.js
+++ teamfesh_react/src/components/header/Nav.js
@@ -1,8 +1,7 @@
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-
-const NavBox = styled.nav`
+const NavBox = styled.ul`
   float: right;
   transition: all 0.2s ease 0s;
   margin: 16px 0px 0px;
@@ -26,9 +25,13 @@ const NavBox = styled.nav`
     transition: all 0.1s ease 0s;
   }
   .nav-list:hover::before {
-    width:100%
+    width: 100%;
   }
-  span {
+
+  .nav-list:hover .nsubmenu {
+    display: block;
+  }
+  .ntext {
     font-weight: 400;
     font-size: 17px;
     line-height: 30px;
@@ -39,89 +42,128 @@ const NavBox = styled.nav`
   .nsubmenu {
     position: absolute;
     width: 140px;
-    top: 50px;
+    top: 17px;
     left: 50%;
     margin-left: -70px;
-    box-shadow: rgb(0 0 0 / 15%) 2px 2px 2px;
+    display: none;
 
+    ul {
+      width: 140px;
+      margin: 32.5px 0 0;
+      padding: 0;
+      background: rgb(0 0 0 / 50%);
+      box-shadow: 0 2px 2px rgb(0 0 0 / 30%);
+      transition: all 0.2s ease;
+      -webkit-transition: all 0.2s ease;
+    }
+
+    li {
+      width: 100%;
+      text-align: center;
+      padding: 5px 0;
+      margin: 0;
+      cursor: pointer;
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 45px;
+      color: rgb(255 255 255);
+      text-decoration: none;
+      &:hover {
+        background: rgba(0, 0, 0, 0.5);
+      }
+    }
   }
-.nsub-list {
-  display: table;
-  width: 140px;
-  background: rgba(0, 0, 0, 0.5);
-  transition: all 0.2s ease 0s;
-  margin-block-start: 0px;
-}
-
-.nsub-list span {
-  display:table-cell;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 40px;
-    color: rgb(255, 255, 255);
-    text-decoration: none;
-}
 `;
 
 const Nav = () => {
   return (
     <>
       <NavBox>
-            {/* ----------01.회사소개------------ */}
-            <Link to='/business' className='nav-list'>
-              <span className='ntext'>회사소개</span>
-              <div className='nsubmenu' id='nsubmenu01'></div>
-            </Link>
+        {/* ----------01.회사소개------------ */}
+        <li className='nav-list'>
+          <Link to='/business'>
+            <span className='ntext'>회사소개</span>
+            <div className='nsubmenu' id='nsubmenu01'></div>
+          </Link>
+        </li>
 
-            {/* ----------02.서비스소개------------ */}
-            <Link to='/service' className='nav-list'>
-              <span className='ntext'>서비스소개</span>
-              <div className='nsubmenu' id='nsubmenu02'>
-                <Link to='/service' className='nsub-list'>
-                  <span>물류</span>
-                </Link>
-                <Link to='/service/distribution' className='nsub-list'>
-                  <span>유통</span>
-                </Link>
-                <Link to='/service/franchise' className='nsub-list'>
-                  <span>프랜차이즈</span>
-                </Link>
-                <Link to='/service/insurance' className='nsub-list'>
-                  <span>보험</span>
-                </Link>
-              </div>
-            </Link>
+        {/* ----------02.서비스소개------------ */}
+        <li className='nav-list'>
+          <Link to='/service'>
+            <span className='ntext'>서비스소개</span>
+            <div className='nsubmenu' id='nsubmenu02'>
+              <ul>
+                <li className='nsub-list'>
+                  <Link to='/service'>
+                    <span>물류</span>
+                  </Link>
+                </li>
+                <li className='nsub-list'>
+                  <Link to='/service/distribution'>
+                    <span>유통</span>
+                  </Link>
+                </li>
+                <li className='nsub-list'>
+                  <Link to='/service/franchise'>
+                    <span>프랜차이즈</span>
+                  </Link>
+                </li>
+                <li className='nsub-list'>
+                  <Link to='/service/insurance'>
+                    <span>보험</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </Link>
+        </li>
 
-            {/* ----------03.인재채용------------ */}
-            <Link to='/recruit' className='nav-list'>
-              <span className='ntext'>인재채용</span>
-              <div className='nsubmenu' id='nsubmenu03'></div>
-            </Link>
+        {/* ----------03.인재채용------------ */}
+        <li className='nav-list'>
+          <Link to='/recruit'>
+            <span className='ntext'>인재채용</span>
+            <div className='nsubmenu' id='nsubmenu03'></div>
+          </Link>
+        </li>
 
-            {/* ----------04.고객지원------------ */}
-            <Link to='/customer' className='nav-list'>
-              <span className='ntext'>고객지원</span>
-              <div className='nsubmenu' id='nsubmenu04'>
-                <Link to='/customer'className='nsub-list'>
-                  <span>공지사항</span>
-                </Link>
-                <Link to='/customer/timfnews'className='nsub-list'>
-                  <span>팀프뉴스</span>
-                </Link>
-                <Link to='/customer/inquiry'className='nsub-list'>
-                  <span>문의하기</span>
-                </Link>
-                <Link to='/customer/fag'className='nsub-list'>
-                  <span>자주 묻는 질문</span>
-                </Link>
-              </div>
-            </Link>
-    
-            {/* ----------05.배송지역검색------------ */}
-            <Link to='/delivery' className='nav-list'>
-              <span className='ntext'>배송 지역 검색</span>
-              <div className='nsubmenu' id='nsubmenu05'></div>
-            </Link>
+        {/* ----------04.고객지원------------ */}
+        <li className='nav-list'>
+          <Link to='/customer'>
+            <span className='ntext'>고객지원</span>
+            <div className='nsubmenu' id='nsubmenu04'>
+              <ul>
+                <li className='nsub-list'>
+                  <Link to='/customer'>
+                    <span>공지사항</span>
+                  </Link>
+                </li>
+                <li className='nsub-list'>
+                  <Link to='/customer/timfnews'>
+                    <span>팀프뉴스</span>
+                  </Link>
+                </li>
+                <li className='nsub-list'>
+                  <Link to='/customer/inquiry'>
+                    <span>문의하기</span>
+                  </Link>
+                </li>
+                <li className='nsub-list'>
+                  <Link to='/customer/fag'>
+                    <span>자주 묻는 질문</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </Link>
+        </li>
+
+        {/* ----------05.배송지역검색------------ */}
+        <li className='nav-list'>
+          <Link to='/delivery'>
+            <span className='ntext'>배송 지역 검색</span>
+            <div className='nsubmenu' id='nsubmenu05'></div>
+          </Link>
+        </li>
       </NavBox>
     </>
   );
